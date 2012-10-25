4d4286bba656338b869774f2f110d63c4d51bb54
Kevin Faaborg <zootella@zootella.com>
2012-10-25 00:19:18 -0400

    new close and pulse done

--- source/org/zootella/base/state/Close.java
+++ source/org/zootella/base/state/Close.java
@@ -1,14 +1,8 @@
 package org.zootella.base.state;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.swing.SwingUtilities;
-
 import org.zootella.base.exception.ProgramException;
 import org.zootella.base.process.Mistake;
 import org.zootella.base.time.Now;
-import org.zootella.base.user.Describe;
 
 /** Have your object extend Close so the program will pulse it, and notice if you forget to later call its close() method. */
 public abstract class Close {
@@ -23,7 +17,7 @@ public abstract class Close {
 
 	/**
 	 * Override this method, and the program will call it periodically.
-	 * Notice things inside your object that have changed or finished, and do the next steps to move forward.
+	 * Notice things inside your object that have changed or finished, and do the next step to move forward.
 	 */
 	public void pulse() {}
 	
@@ -40,8 +34,8 @@ public abstract class Close {
 	 * This automatically runs before execution enters the constructor of an object that extends Close.
 	 */
 	public Close() {
-		requireEventThread(); // Only the event thread can make a new object
-		add(this); // Add this new object that extends Close to the program's list of open objects
+		Pulse.requireEventThread(); // Only the event thread can make a new Close object
+		Pulse.add(this); // Add this new object that extends Close to the program's list of open objects
 	}
 	
 	/** true once this object that extends Close has been closed, and promises to not change again. */
@@ -50,7 +44,7 @@ public abstract class Close {
 
 	/**
 	 * Mark this object that extends Close as closed, and only do this once.
-	 * Start your close() method with the code if (already()) return;.
+	 * Start your close() method with the code "if (already()) return;".
 	 * The first time already() runs, it marks this object as closed and returns false.
 	 * Try calling it again, and it will just return true.
 	 */
@@ -76,7 +70,7 @@ public abstract class Close {
 
 	/** Close c ignoring null and exceptions. */
 	public static void close(Close c) {
-		requireEventThread(); // Only the event thread can close an object
+		Pulse.requireEventThread(); // Only the event thread can close an object
 		if (c == null) return;
 		try { c.close(); } catch (Throwable t) { Mistake.log(t); } // Keep going to close the next object
 	}
@@ -89,153 +83,6 @@ public abstract class Close {
 	public static boolean open(Close c) { return c != null && !c.closed(); }
 	/** true if c exists and is closed. */
 	public static boolean done(Close c) { return c != null && c.closed(); }
-	
-	// Pulse
-	
-	/** true when we've set Java to call run(), and it hasn't yet. */
-	private static boolean start;
-	/** true when an object has requested another pass up the pulse list. */
-	private static boolean again;
-
-	/**
-	 * An object in the program has changed or finished.
-	 * Pulse up the list soon so the object that made it can notice and take the next step forward.
-	 */
-	public static void pulseSoon() {
-		requireEventThread(); // Only the event thread can request a pulse soon
-
-		// Start a pulse if one isn't already happening
-		if (!start) { // No need to start a new pulse if we're doing one now already
-			start = true;
-
-			SwingUtilities.invokeLater(new Runnable() { // This code calls invokeLater() and returns immediately
-				public void run() { // Soon after, the same thread but a separate event calls this run() method
-					try {
-						pulseAll();
-					} catch (Throwable t) { Mistake.stop(t); } // Stop the program for an exception we didn't expect
-				}
-			});
-		}
-
-		// Have the pulse loop up the list again
-		again = true;
-	}
-
-	/** Pulse all the open objects in the program until none request another pulse soon. */
-	private static void pulseAll() {
-		
-		countPulses++;
-		countTimeOutsidePulse += now.age();
-		now = new Now();
-		
-		// Pulse up the list in many passes until no object requests another pulse soon
-		while (again) {
-			again = false;
-			
-			if (now.expired(pulseLimit)) { countLimits++; break; }
-			
-			// Pulse up the list in a single pass
-			for (int i = list.size() - 1; i >= 0; i--) { // Loop backwards to pulse contained objects before the older objects that made them
-				countLoops++;
-				Close c = list.get(i);
-				if (!c.closed()) { // Skip closed objects
-					try {
-						countObjects++;
-						c.pulse(); // Pulse the object so it notices things that have finished and moves to the next step
-					} catch (Throwable t) { Mistake.stop(t); } // Stop the program for an exception we didn't expect
-				}
-			}
-		}
-
-		// In a single pass after that, pulse up the list to have objects compose information for the user
-		for (int i = list.size() - 1; i >= 0; i--) {
-			Close c = list.get(i);
-			if (!c.closed()) { // Skip closed objects
-				try {
-					c.pulseUser(); // Pulse the object to have it compose text for the user to show current information
-				} catch (Throwable t) { Mistake.stop(t); } // Stop the program for an exception we didn't expect
-			}
-		}
-		
-		clear(); // Remove closed objects from the list all at once at the end
-		start = false; // Allow the next call to pulse soon to start a new pulse
-		
-		countTimeInsidePulse += now.age();
-		now = new Now();
-	}
-
-	/** Add a new object that extends Close to the program's list of open objects. */
-	private static void add(Close c) {
-		list.add(c); // Adding on the end is OK because we loop by index number
-	}
-
-	/** Remove objects that got closed from our list. */
-	private static void clear() {
-		for (int i = list.size() - 1; i >= 0; i--) { // Loop backwards so we can remove things along the way
-			if (list.get(i).closed()) list.remove(i);
-		}
-	}
-
-	/** The program's list of all objects that extend Close, and aren't closed yet. */
-	private static final List<Close> list = new ArrayList<Close>();
-
-	// Monitor
-
-	/** The program pulses 5 times a second. */
-	public static final int pulseInterval = 200;
-	/** Don't let a single pulse last more than 1/10 second. */
-	public static final int pulseLimit = 100;
-
-	/** The time when we last entered or left the pulse function. */
-	private static Now now = new Now();
-	
-	/** How many pulses have happened. */
-	private static long countPulses;
-	/** How many loops have happened within all the pulses. */
-	private static long countLoops;
-	/** How many objects we've pulsed within all the loops and pulses. */
-	private static long countObjects;
-	/** How many pulses have gone over the time limit and quit early. */
-	private static long countLimits;
-	/** How long the program has spent inside the pulse function, in milliseconds. */
-	private static long countTimeInsidePulse;
-	/** How long the program has spent outside the pulse function, in milliseconds. */
-	private static long countTimeOutsidePulse;
-
-	/** Compose text about how efficiently the program has been running. */
-	public static String composeEfficiency() {
-		
-		StringBuffer s = new StringBuffer();
-		s.append("The average pulse looped up a list of [" + Describe.average(countLoops, countObjects) + "] objects [" + Describe.average(countPulses, countLoops) + "] times.");
-		s.append("The average pulse took [" + Describe.average(countPulses, countTimeInsidePulse) + "] milliseconds, and [" + Describe.percent(countLimits, countPulses) + "] hit the time limit.");
-		s.append("The program spent [" + Describe.percent(countTimeInsidePulse, countTimeInsidePulse + countTimeOutsidePulse) + "] of its time pulsing.");
-		return s.toString();
-	}
-	
-	/**
-	 * Call before the program exits to make sure we've closed every object.
-	 * @return Text about objects still open by mistake, or blank if there's no problem
-	 */
-	public static String confirmAllClosed() {
-		
-		clear(); // Remove closed objects from the list
-		
-		int size = list.size();
-		if (size == 0) return ""; // Good, we had closed them all already
-		
-		StringBuffer s = new StringBuffer(); // Compose and return text about the objects still open by mistake
-		s.append(size + " objects open:\n");
-		for (Close c : list)
-			s.append(c.toString() + "\n");
-		return s.toString();
-	}
-	
-	// Require
-
-	/** Make sure this is the event thread, or exit the program without returning. */
-	private static void requireEventThread() {
-		if (!SwingUtilities.isEventDispatchThread()) Mistake.stop(new ProgramException("method limited to event thread only"));
-	}
 
 	// Log
 
---
+++ source/org/zootella/base/state/Pulse.java
@@ -0,0 +1,187 @@
+package org.zootella.base.state;
+
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.AbstractAction;
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
+
+import org.zootella.base.exception.ProgramException;
+import org.zootella.base.process.Mistake;
+import org.zootella.base.time.Now;
+import org.zootella.base.user.Describe;
+
+public class Pulse {
+	
+	// Presets
+
+	/** The program pulses 5 times a second. */
+	public static final int timeInterval = 200;
+	/** Don't let a single pulse last more than 1/10 second. */
+	public static final int timeLimit = 100;
+	
+	// Timer
+
+	// When the timer we make below goes off, Java calls this method
+	private static class MyActionListener extends AbstractAction {
+		public void actionPerformed(ActionEvent a) {
+			try {
+				soon();
+			} catch (Throwable t) { Mistake.stop(t); } // Stop the program for an exception we didn't expect
+		}
+	}
+
+	// Start
+	
+	/** true when we've set Java to call run(), and it hasn't yet. */
+	private static boolean start;
+	/** true when an object has requested another pass up the pulse list. */
+	private static boolean again;
+
+	/**
+	 * An object in the program has changed or finished.
+	 * Pulse soon so the object that made it can notice and take the next step forward.
+	 */
+	public static void soon() {
+		requireEventThread(); // Only the event thread can request a pulse soon
+
+		// Start a pulse if one isn't already happening
+		if (!start) { // No need to start a new pulse if we're doing one now already
+			start = true;
+			SwingUtilities.invokeLater(new MyRunnable()); // Have Java call run() below separately and soon
+		}
+
+		// Have the pulse loop up the list again
+		again = true;
+	}
+
+	// Soon after soon() above calls SwingUtilities.invokeLater(), Java calls this run() method
+	private static class MyRunnable implements Runnable {
+		public void run() {
+			try {
+				pulse();
+			} catch (Throwable t) { Mistake.stop(t); } // Stop the program for an exception we didn't expect
+		}
+	}
+
+	// Pulse
+	
+	/** Pulse all the open objects in the program until none request another pulse soon. */
+	private static void pulse() {
+		
+		countPulses++;
+		countTimeOutside += now.age();
+		now = new Now();
+		
+		// Pulse up the list in many passes until no object requests another pulse soon
+		while (again) {
+			again = false;
+			
+			if (now.expired(timeLimit)) { countHitLimit++; break; } // Quit early if we're over the time limit
+			
+			// Pulse up the list in a single pass
+			for (int i = list.size() - 1; i >= 0; i--) { // Loop backwards to pulse contained objects before the older objects that made them
+				countLoops++;
+				Close c = list.get(i);
+				if (!c.closed()) { // Skip closed objects
+					countObjects++;
+					try {
+						c.pulse(); // Pulse the object so it notices things that have finished and moves to the next step
+					} catch (Throwable t) { Mistake.stop(t); } // Stop the program for an exception we didn't expect
+				}
+			}
+		}
+
+		// In a single pass after that, pulse up the list to have objects compose information for the user
+		for (int i = list.size() - 1; i >= 0; i--) {
+			Close c = list.get(i);
+			if (!c.closed()) { // Skip closed objects
+				try {
+					c.pulseUser(); // Pulse the object to have it compose text for the user to show current information
+				} catch (Throwable t) { Mistake.stop(t); } // Stop the program for an exception we didn't expect
+			}
+		}
+		
+		clear(); // Remove closed objects from the list all at once at the end
+		start = false; // Allow the next call to soon to start a new pulse
+		
+		Timer timer = new Timer(timeInterval, new MyActionListener()); // Set a new timer to go off once
+		timer.setRepeats(false);
+		timer.start();
+		
+		countTimeInside += now.age();
+		now = new Now();
+	}
+	
+	// List
+
+	/** Add a new object that extends Close to the program's list of open objects. */
+	public static void add(Close c) { // The Close constructor already checked that only the event thread can do this
+		list.add(c); // It's safe to add to the end even during a pulse because we loop by index number
+	}
+
+	/** Remove objects that got closed from our list. */
+	private static void clear() {
+		for (int i = list.size() - 1; i >= 0; i--) { // Loop backwards so we can remove things along the way
+			if (list.get(i).closed()) list.remove(i);
+		}
+	}
+
+	/** The program's list of all objects that extend Close, and aren't closed yet. */
+	private static final List<Close> list = new ArrayList<Close>();
+	
+	// Monitor
+
+	/** The time when we last entered or left the pulse function. */
+	private static Now now = new Now();
+	
+	/** How many pulses have happened. */
+	private static long countPulses;
+	/** How many loops have happened within all the pulses. */
+	private static long countLoops;
+	/** How many objects we've pulsed within all the loops and pulses. */
+	private static long countObjects;
+	/** How many pulses have gone over the time limit and quit early. */
+	private static long countHitLimit;
+	/** How long the program has spent inside the pulse function, in milliseconds. */
+	private static long countTimeInside;
+	/** How long the program has spent outside the pulse function, in milliseconds. */
+	private static long countTimeOutside;
+
+	/** Compose text about how efficiently the program has been running. */
+	public static String composeEfficiency() {
+		
+		StringBuffer s = new StringBuffer();
+		s.append("The average pulse looped up a list of [" + Describe.average(countLoops, countObjects) + "] objects [" + Describe.average(countPulses, countLoops) + "] times.");
+		s.append("The average pulse took [" + Describe.average(countPulses, countTimeInside) + "] milliseconds, and [" + Describe.percent(countHitLimit, countPulses) + "] hit the time limit.");
+		s.append("The program spent [" + Describe.percent(countTimeInside, countTimeInside + countTimeOutside) + "] of its time pulsing.");
+		return s.toString();
+	}
+	
+	/**
+	 * Call before the program exits to make sure we've closed every object.
+	 * @return Text about objects still open by mistake, or blank if there's no problem
+	 */
+	public static String confirmAllClosed() {
+		
+		clear(); // Remove closed objects from the list
+		
+		int size = list.size();
+		if (size == 0) return ""; // Good, we had closed them all already
+		
+		StringBuffer s = new StringBuffer(); // Compose and return text about the objects still open by mistake
+		s.append(size + " objects open:\n");
+		for (Close c : list)
+			s.append(c.toString() + "\n");
+		return s.toString();
+	}
+	
+	// Require
+
+	/** Make sure this is the event thread, or exit the program without returning. */
+	public static void requireEventThread() {
+		if (!SwingUtilities.isEventDispatchThread()) Mistake.stop(new ProgramException("method limited to event thread only"));
+	}
+}
