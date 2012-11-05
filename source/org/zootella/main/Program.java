7b97be277700cc56ec4e21de85e4a1796f3ef85e
Kevin Faaborg <zootella@zootella.com>
2012-11-04 23:40:17 -0500

    program makes ding

--- source/org/zootella/main/Program.java
+++ source/org/zootella/main/Program.java
@@ -2,6 +2,8 @@ package org.zootella.main;
 
 import org.zootella.base.process.Mistake;
 import org.zootella.base.state.Close;
+import org.zootella.base.state.Ding;
+import org.zootella.base.state.Pulse;
 import org.zootella.base.store.Store;
 import org.zootella.base.user.Face;
 
@@ -14,18 +16,26 @@ public class Program extends Close {
 		store = new Store(); // Load data and preferences from last time the program ran
 		core = new Core(this); // Make the core that does everything
 		user = new User(this); // Put the window on the screen to let the user interact with it
+		
+		ding = new Ding();
 	}
 	
 	public final Store store;
 	public final Core core;
 	public final User user;
+	
+	private final Ding ding;
 
 	@Override public void close() {
 		if (already()) return;
 		
+		close(ding);
 		close(user);
 		close(core);
+		
 		store.save();
+		
 		Mistake.closeCheck();
+		log(Pulse.pulse.composeEfficiency());
 	}
 }
