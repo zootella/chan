0a4c84fb1600aa4acf051240bf8effd4c89278ee
Kevin Faaborg <zootella@zootella.com>
2013-01-20 15:15:36 -0500

    before value

--- source/org/zootella/main/Snippet.java
+++ source/org/zootella/main/Snippet.java
@@ -9,6 +9,20 @@ public class Snippet {
 		
 		Data d = new Data("hello\r\n");
 		Log.log(d.quote());
+		
+		
+		
+		
+		//see what it's like to encrypt and decrypt really small bits of data
+		//small files will be like this, as well as small collections
+		//for instance, the hash library of every sub1mb file will be just 20 bytes
+		//do you need a salt or soemthing to protect that?
+		
+		
+		
+		
+		
+		
 	}
 }
 	
